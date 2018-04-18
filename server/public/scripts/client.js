e8f86d4e854e7711317f2811946354711a024c78
nkean <nathankean2@gmail.com>
2018-04-18 15:22:03 -0500

    added button and handler

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,13 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jQuery is loaded');
+    $.ajax({
+        type: 'GET',
+        url: '/all-quotes'
+    })
+    .then(function(response) {
+        console.log(response);
+    });
+
+    $('#newQuoteButton').on('click', changeQuote);
 }

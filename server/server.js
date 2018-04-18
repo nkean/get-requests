9b462e134f051544c8b981e5ade08d191e319ea6
nkean <nathankean2@gmail.com>
2018-04-18 10:17:09 -0500

    Change listening function to es6 arrow format

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-app.listen(PORT, function () {
+app.listen(PORT, () => {
     console.log(`listening on port: ${PORT}`);
 });
 
